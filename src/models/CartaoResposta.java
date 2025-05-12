c02b53299183b6b0a1f9ea9937f9d2806e9d429e
Valadyne <caio.marinho@ufrpe.br>
2025-05-12 20:49:53 -0300

    Mudanças na classe Alternativa e na (finada) classe Resposta
    - Mudei o nome de 'Resposta' p 'CartaoResposta'
    - Removi os métodos comentados que não vão ser criados//usados neste momento do projeto dentro da classe CartaoResposta
    - adicionei o atributo id com getters e setters em ambas as classes e no UML
    - adicionei o construtor vazio em ambas as classes

---
+++ src/models/CartaoResposta.java
@@ -0,0 +1,30 @@
+package models;
+
+public class CartaoResposta {
+	private int id;
+	private int[] resposta;
+	
+	public CartaoResposta(int id, int[] resposta) {
+		this.id = id;
+		this.resposta = resposta;
+	}
+	
+	public CartaoResposta() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int[] getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(int[] resposta) {
+		this.resposta = resposta;
+	}
+}
