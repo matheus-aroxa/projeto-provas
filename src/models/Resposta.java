916af69cf0cc599c0e204b9f478d281580d517c7
caio-marinho-UFRPE <caio.marinho@ufrpe.br>
2025-05-12 14:30:47 -0300

    usar git é tenebroso  pprt

---
+++ src/models/Resposta.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Resposta {
+	private int[] resposta;
+	
+	public Resposta(int[] resposta) {
+		this.resposta = resposta;
+	}
+
+	public int[] getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(int[] resposta) {
+		this.resposta = resposta;
+	}
+	
+	/*public void responder() {
+		coiso
+	}*/
+	
+	/*public void corrigir() {
+		coiso
+	}*/
+}
