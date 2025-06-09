fdad159918a45dd50f7c3a5a6857e7c1102a4d0b
davyusow <dayvsonf27@gmail.com>
2025-06-09 15:55:50 -0300

    Ajustes Administrador e Repositório de Administradores

--- src/models/usuarios/Administrador.java
+++ src/models/usuarios/Administrador.java
@@ -6,4 +6,7 @@ public class Administrador extends Usuario {
     public Administrador(int id, String nome, int cpf, LocalDate dataDeNascimento, String email, String senha) {
         super(id, nome, cpf, dataDeNascimento, email, senha);
     }
+    public Administrador() {
+        super();
+    }
 }
--- src/teste.java
+++ src/teste.java
@@ -1,31 +1,14 @@
 import models.provas.Questao;
+import repos.RepositorioAdminstrador;
 import repos.RepositorioQuestoes;
 
 public class teste {
     public static void main(String[] args) {
-//        RepositorioQuestoes dados = new RepositorioQuestoes().criaRepositorio(100);
-//        if(dados.adicionarQuestao(new Questao(1,"Caça pombo",new int[]{1,2,3,4})))
-//            System.out.printf("Adicionado com sucesso!\n");;
-//        if(dados.buscarQuestao(1) != -1)
-//            System.out.printf("encontrado com sucesso!\n");
-//        if(dados.removerQuestao(1))
-//            System.out.println("Removido com sucesso!");
+        RepositorioAdminstrador repositorioAdminstrador = RepositorioAdminstrador.getInstancia(10);
+        RepositorioAdminstrador repositorioAdminstrador2 = RepositorioAdminstrador.getInstancia(10);
 
+        System.out.println(repositorioAdminstrador);
+        System.out.println(repositorioAdminstrador2);
 
-
-        RepositorioQuestoes repo = RepositorioQuestoes.criar(10);
-        RepositorioQuestoes repo2 = RepositorioQuestoes.criar(13);
-        RepositorioQuestoes repo3 = RepositorioQuestoes.criar(14);
-        RepositorioQuestoes repo4 = RepositorioQuestoes.criar(17);
-
-        System.out.println(repo);
-        System.out.println(repo2);
-        System.out.println(repo3);
-        System.out.println(repo4);
-
-        repo.adicionar(new Questao(1,"adedonha",null));
-        repo.adicionar(new Questao(2,"pau no gato",null));
-        repo.adicionar(new Questao(3,"fila indiana",null));
-        repo.listar();
     }
 }
