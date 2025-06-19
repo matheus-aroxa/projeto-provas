9c6683221ca82f95c24086530493d8dfda4ce2fd
davyusow <dayvsonf27@gmail.com>
2025-06-18 22:47:58 -0300

    Correção do cadastroProfessor

--- src/controller/CadastroProfessor.java
+++ src/controller/CadastroProfessor.java
@@ -2,20 +2,16 @@ package controller;
 
 import models.usuarios.professor.Professor;
 import repos.RepositorioUsuarios;
-
 import java.time.LocalDate;
 
 
 public class CadastroProfessor{
 
     public Professor cadastro(RepositorioUsuarios repo, int id, String nome, int cpf, LocalDate dataDeNascimento, String email, String senha, int idTurma) {
-        if (repo.procurarUsuario(id) != -1) {
-            String emailValido = "([a-zA-Z0-9\\.-_])+@([a-zA-Z])+(\\.([a-zA-Z])+)+"; //válida e-mails
-            if(email.matches(emailValido)) {   //email é valido
-                //validar cpf
-                //validar se a turma existe
-                return new Professor(id, nome, cpf, dataDeNascimento, email, senha, idTurma);
-            }
+        String emailValido = "([a-zA-Z0-9\\.-_])+@([a-zA-Z])+(\\.([a-zA-Z])+)+"; //válida e-mails
+        int validaCpf = String.valueOf(Math.abs(cpf)).length(); //retorna o número de dígitos do cpf
+        if(repo.procurarUsuario(id) != -1 && email.matches(emailValido) && validaCpf == 11 && senha != null && dataDeNascimento != null) {
+            return new Professor(id, nome, cpf, dataDeNascimento, email, senha, idTurma);
         }
         return null;
     }
