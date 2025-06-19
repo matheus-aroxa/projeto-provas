863e6ca578cd1b5eb698f8e606938d607c2f016c
Valadyne <caio.marinho@ufrpe.br>
2025-06-19 18:08:03 -0300

    Começo da refatoração dos cadastros, será implementada melhor posteriormente

--- src/controller/CadastroAdmin.java
+++ src/controller/CadastroAdmin.java
@@ -1,23 +1,21 @@
 package controller;
 
-import java.time.LocalDate;
-
 import models.usuarios.Administrador;
 import repos.RepositorioUsuarios;
 
 public class CadastroAdmin {
 	
-	public static Administrador cadastro(RepositorioUsuarios repo, int id, String nome, long cpf, LocalDate dataDeNascimento, String email, String senha) {
+	public static Administrador cadastro(RepositorioUsuarios repositorio, Administrador admin) {
 		String emailValido = "([a-zA-Z0-9\\.-_])+@([a-zA-Z])+(\\.([a-zA-Z])+)+"; //válida e-mails
-		int validaCpf = String.valueOf(Math.abs(cpf)).length(); //retorna o número de dígitos do cpf
-		if (repo.procurarUsuario(id) == -1
-				&& email.matches(emailValido)
+		int validaCpf = String.valueOf(Math.abs(admin.getCpf())).length(); //retorna o número de dígitos do cpf
+		if(repositorio.procurarUsuario(admin.getId()) == -1
+				&& admin.getEmail().matches(emailValido)
 				&& validaCpf == 11
-                && senha != null
-                && dataDeNascimento != null) {
-            return new Administrador(id, nome, cpf, dataDeNascimento, email, senha);
-            
+				&& admin.getSenha() != null
+				&& admin.getDataDeNascimento() != null) {
+			return new Administrador(admin.getId(), admin.getNome(), admin.getCpf(), admin.getDataDeNascimento(), admin.getEmail(), admin.getSenha());
+			
 		}
-		return null; 
+		return null;
 	}
 }
--- src/controller/CadastroAluno.java
+++ src/controller/CadastroAluno.java
@@ -1,21 +1,19 @@
 package controller;
 
-import java.time.LocalDate;
-
 import models.usuarios.Aluno;
 import repos.RepositorioUsuarios;
 
 public class CadastroAluno {
 	
-	public static Aluno cadastro(RepositorioUsuarios repo, int id, String nome, long cpf, LocalDate dataDeNascimento, String email, String senha, int idTurma) {
+	public static Aluno cadastro(RepositorioUsuarios repositorio, Aluno aluno) {
 		String emailValido = "([a-zA-Z0-9\\.-_])+@([a-zA-Z])+(\\.([a-zA-Z])+)+"; //válida e-mails
-        int validaCpf = String.valueOf(Math.abs(cpf)).length(); //retorna o número de dígitos do cpf
-		if (repo.procurarUsuario(id) == -1
-				&& email.matches(emailValido)
-				&& validaCpf == 11
-				&& senha != null
-				&& dataDeNascimento != null) {
-			return new Aluno(id, nome, cpf, dataDeNascimento, email, senha, idTurma);
+        int validaCpf = String.valueOf(Math.abs(aluno.getCpf())).length(); //retorna o número de dígitos do cpf
+        if(repositorio.procurarUsuario(aluno.getId()) == -1
+        		&& aluno.getEmail().matches(emailValido)
+        		&& validaCpf == 11
+        		&& aluno.getSenha() != null
+        		&& aluno.getDataDeNascimento() != null) {
+        	return new Aluno(aluno.getId(), aluno.getNome(), aluno.getCpf(), aluno.getDataDeNascimento(), aluno.getEmail(), aluno.getSenha(), aluno.getIdTurma());
         }
 		return null;
 	}
