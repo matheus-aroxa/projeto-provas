0e66e4946afcad41f168cb8e0ba7386db7b8762f
davyusow <dayvsonf27@gmail.com>
2025-06-25 22:50:03 -0300

    ajustes dos repositórios

--- src/repos/RepositorioProvas.java
+++ src/repos/RepositorioProvas.java
@@ -1,52 +1,49 @@
-//package repos;
-//
-//import models.Turma;
-//import models.provas.CartaoResposta;
-//import models.provas.Prova;
-//import models.provas.Questao;
-//
-//import java.time.LocalDateTime;
-//import java.time.Period;
-//import java.util.concurrent.atomic.AtomicInteger;
-//
-//public class RepositorioProvas {
-//    private Prova[] provas;
-//    private int tam;
-//    private static RepositorioProvas instance;
-//    private static AtomicInteger contador = new AtomicInteger();
-//
-//    public static RepositorioProvas getInstance(int tam) {
-//        if(instance == null) {
-//            instance = new RepositorioProvas(tam);
-//        }
-//        return instance;
-//    }
-//
-//    private RepositorioProvas(int tam) {
-//    	this.provas = new Prova[tam];
-//    	this.tam = 0;
-//    }
-//
-//    public void adicionar(String titulo, String descricao, LocalDateTime dataAplicacao, Period duracao, boolean isRemoto, Turma[] turmas, Questao[] questoes, CartaoResposta[] respostas) {
-//        if(tam < provas.length) {
-//            this.provas[this.tam] = CadastroProva.cadastro(instance,contador.incrementAndGet(),titulo,descricao,dataAplicacao,duracao,isRemoto,turmas,questoes,respostas);
-//            this.tam++;
-//        }
-//    }
-//
-//    public Prova[] getAllProvas() {
-//        return provas;
-//    }
-//
-//    public Prova getProvaById(int id) {
-//        for(int i = 0; i < tam; i++) {
-//            if(provas[i].getId() == id) {
-//                return provas[i];
-//            }
-//        }
-//        return null;
-//    }
-//
+package repos;
+
+import models.Turma;
+import models.provas.CartaoResposta;
+import models.provas.Prova;
+import models.provas.Questao;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RepositorioProvas {
+
+    private Prova[] provas;
+    private static AtomicInteger contador = new AtomicInteger();
+    private static RepositorioProvas instance;
+    private int tam;
+
+    private RepositorioProvas(int tam) {
+        this.provas = new Prova[tam];
+        this.tam = 0;
+    }
+
+    public static RepositorioProvas getInstance(int tam) {
+        if(instance == null) {
+            instance = new RepositorioProvas(tam);
+        }
+        return instance;
+    }
+
+    public void adicionar(String titulo, String descricao, LocalDateTime dataAplicacao, Period duracao, boolean isRemoto, Turma[] turmas, Questao[] questoes, CartaoResposta[] respostas) {
+        provas[contador.get()] = new Prova(contador.getAndIncrement(),titulo,  descricao,  dataAplicacao,  duracao,  isRemoto, turmas,  questoes, respostas);
+    }
+
+    public Prova[] getAllProvas() {
+        return provas;
+    }
+
+    public Prova procurar(int id) {
+        for(int i = 0; i < tam; i++) {
+            if(provas[i].getId() == id) {
+                return provas[i];
+            }
+        }
+        return null;
+    }
+
 //    public Prova getProvaByTitulo(String titulo) {
 //    	for(int i = 0; i < tam; i++) {
 //    		if(provas[i].getTitulo().equals(titulo)) {
@@ -73,22 +70,22 @@
 //    	}
 //    	return null;
 //    }
-//
-//    public Prova deletar(int id) {
-//        for(int i = 0; i < tam; i++) {
-//            if(provas[i].getId() == id) {
-//                Prova provaR = provas[i];
-//                for(int j = i; j < tam - 1; j++) {
-//                    provas[j] = provas[j + 1];
-//                }
-//                provas[tam - 1] = null;
-//                tam--;
-//                return provaR;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    public void editarProva(Prova prova) {};
-//
-//}
+
+    public Prova remover(int id) {
+        for(int i = 0; i < tam; i++) {
+            if(provas[i].getId() == id) {
+                Prova provaR = provas[i];
+                for(int j = i; j < tam - 1; j++) {
+                    provas[j] = provas[j + 1];
+                }
+                provas[tam - 1] = null;
+                tam--;
+                return provaR;
+            }
+        }
+        return null;
+    }
+
+    public void editarProva(Prova prova) {};
+
+}
