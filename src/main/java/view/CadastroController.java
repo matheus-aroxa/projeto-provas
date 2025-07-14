0dec5299de99cd096e7ec2c77e8082da19c67fda
davyusow <dayvsonf27@gmail.com>
2025-07-14 17:49:43 -0300

    Implementação do choice box na tela de cadastro

--- src/main/java/view/CadastroController.java
+++ src/main/java/view/CadastroController.java
@@ -4,22 +4,38 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CadastroController {
+public class CadastroController implements Initializable {
 
     @FXML
-    Stage janela;
+    private Stage janela;
     @FXML
-    Scene aba;
+    private Scene aba;
     @FXML
-    Parent root;
+    private Parent root;
+    @FXML
+    private ChoiceBox<String> escolhaUsuario;
+    private String[] tipos = {"Administrador","Aluno","Professor"};
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        escolhaUsuario.getItems().addAll(tipos);
+    }
+
+    void getTipos(ActionEvent evento){
+        String atual = escolhaUsuario.getValue();
+    }
 
     @FXML
     void irLogin(ActionEvent evento) throws IOException {
