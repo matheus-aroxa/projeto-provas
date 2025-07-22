e411e3fa7d505132e542bb4dd79355016a4d9cfa
miromorii <matheusaroxa13@gmail.com>
2025-07-21 22:31:10 -0300

    login

--- src/main/java/view/GerenciarRelatoriosController.java
+++ src/main/java/view/GerenciarRelatoriosController.java
@@ -1,9 +1,5 @@
 package view;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 
 public class GerenciarRelatoriosController extends FuncoesComuns {
 
